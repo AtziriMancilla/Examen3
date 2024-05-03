8a2c327d2d5308f8053154c0954bd1eec06bcd92
Atziri Mancilla <atzirimc22@gmail.com>
2024-05-03 11:43:11 -0600

    tostring persona

--- src/Usuarios/Persona.java
+++ src/Usuarios/Persona.java
@@ -1,4 +1,19 @@
 package Usuarios;
 
 public class Persona {
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String ciudad;
+    private String estado;
+    private String curp;
+    private String dirección;
+    private int anioNacimiento;
+    private String RFC;
+    //private Sucursal sucursal;
+    private String contrasena;
+    public String toString(){
+        return String.format("ID: %d,Nombre: %s, Apellido: %s %s, Ciudad: %s, Estado: %s,Curp: %s, Direccion: %s, Año nacimiento: %d, RFC: %s",nombre,apellidoPaterno,apellidoMaterno,ciudad,estado,curp,dirección,anioNacimiento,RFC);
+    }
 }
