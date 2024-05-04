b0e641768d4c4843b858425886d0e2b2f8d1d36c
Atziri Mancilla <atzirimc22@gmail.com>
2024-05-04 11:27:14 -0600

    formato fecha inversion

--- src/Usuarios/utils/Inversion.java
+++ src/Usuarios/utils/Inversion.java
@@ -3,15 +3,20 @@ package Usuarios.utils;
 import Usuarios.Inversionista;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Inversion {
-    Inversionista inversionista;
-    double monto;
-    LocalDate fecha;
+    private Inversionista inversionista;
+    private double monto;
+    private LocalDate fecha;
     public Inversion(Inversionista inversionista,double monto){
         this.inversionista=inversionista;
         this.monto=monto;
         fecha=LocalDate.now();
     }
-
+    public String mostrarInversion(){
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd/MM/YY hh:mm");
+        String fechaFormateada = fecha.format(pattern);
+        return String.format("Inversionista: %s Monto: %f Fecha: %s ",inversionista.toString(),monto,fechaFormateada);
+    }
 }
