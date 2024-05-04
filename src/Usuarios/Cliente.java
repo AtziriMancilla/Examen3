0fb7bb8e39165cfb96952da3a6c573f7491bbeaf
Atziri Mancilla <atzirimc22@gmail.com>
2024-05-03 20:40:06 -0600

    localdate

--- src/Usuarios/Cliente.java
+++ src/Usuarios/Cliente.java
@@ -1,15 +1,17 @@
 package Usuarios;
 
 
+import java.time.LocalDate;
+
 public class Cliente extends Persona{
     private int id;
-    //private LocalDate fechaRegistro;
+    private LocalDate fechaRegistro;
     //private TarjetaDebito tarjetaDebito;
     //private ArrayList<TarjetaCredito> tarjetasCredito=new ArrayList<>();
 
     public Cliente(String nombre, String apellidoPaterno, String apellidoMaterno, String ciudad, String estado, String curp, String direccion, int anioNacimiento, String RFC, String contrasena) {
         super(nombre, apellidoPaterno, apellidoMaterno, ciudad, estado, curp, direccion, anioNacimiento, RFC, contrasena);
-        //fechaRegistro=LocalDate.now();
+        fechaRegistro=LocalDate.now();
         //generar tarjeta debito
     }
     public static void solicitarTarjeta(){
