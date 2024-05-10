c82bad8d1a65b2dfd4ed4d28362700e419c0947f
JafetSan37 <j.s.b_95@outlook.com>
2024-05-09 21:11:42 -0600

    Respaldo de avances Jafi

--- src/Banco/Menu.java
+++ src/Banco/Menu.java
@@ -1,11 +1,14 @@
 package Banco;
 
+import Banco.Tarjetas.TarjetaDebito;
+import Usuarios.Cliente;
 import Usuarios.Persona;
 import Usuarios.utils.DatosComun;
 import Usuarios.utils.Rol;
 import utils.UsuarioEnSesion;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -63,12 +66,83 @@ public class Menu {
     }
     private static void seleccionarMenu() {
         switch (UsuarioEnSesion.getInstancia().getUsuarioActual().getRol()) {
-            //case CAPTURISTA -> mostrarMenuC();
-            //case CLIENTE -> mostrarMenuTrabajador();
-            //case EJECUTIVO -> mostrarMenuEjecutivo();
-            //case GERENTE -> mostrarMenuGerente();
-            //case INVERSIONISTA -> mostrarMenuInversionista();
+            case CAPTURISTA -> mostrarMenuC();
+            case CLIENTE -> mostrarMenuCliente();
+            case EJECUTIVO -> mostrarMenuEjecutivo();
+            case GERENTE -> mostrarMenuGerente();
+            case INVERSIONISTA -> mostrarMenuInversionista();
         }
+    }
+
+    private static void mostrarMenuCliente() {
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        Cliente cliente = (Cliente)UsuarioEnSesion.getInstancia().getUsuarioActual();//Para obtener al cliente de la sesión.
+        TarjetaDebito tarjetaDebito = cliente.getTarjetaDebito();//Como sólo es una cuenta de débito, se obtiene la tarjeta antes de la selección del usuario.
+        System.out.println("\tMenú Cliente");
+        System.out.println("\nBienvenido. Seleccione una opción:\n");
+        System.out.println("1) Consultar cuenta de débito");
+        System.out.println("2) Depositar a cuenta de débito");
+        System.out.println("3) Retirar dinero");
+        System.out.println("4) Consultar cuentas de crédito");
+        System.out.println("5) Realizar solicitud de tarjeta de crédito");
+        System.out.println("6) Revisar estatus de solicitud de tarjeta");
+        System.out.println("7) Realizar compra con tarjeta de crédito");
+        System.out.println("8) Realizar pago a tarjeta de crédito");
+        System.out.println("9) Cerrar Sesión");
+        do {
+            try {
+                System.out.print("Opción: ");
+                opcion = sc.nextInt();
+                sc.nextLine();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Saldo de la cuenta: "+ tarjetaDebito.getSaldo());
+                        System.out.println(tarjetaDebito.toString());//Obtener el resto de los datos de la tarjeta
+                        break;
+                    case 2:
+                        System.out.println("Este será el menú depósito");
+
+                        break;
+                    case 3:
+                        System.out.println("Este será el menú retiro");
+                        break;
+                    case 4:
+                        System.out.println("Este será el menú consulta crédito");
+                        break;
+                    case 5:
+                        System.out.println("\tBienvenido\n");
+                        Cliente.solicitarTarjetaCredito(cliente);
+                        break;
+                    case 6:
+                        System.out.println("Este será el menú status de solicitud de tarjeta");
+                        break;
+                    case 7:
+                        System.out.println("Este será el menú realizar compra con tarjeta");
+                        break;
+                    case 8:
+                        System.out.println("Este será el menú realizar pago a tarjeta");
+                        break;
+                    case 9:
+                        System.out.println("Cerrando Sesión...");
+                        flag = false;
+                        break;
+                    default:
+                        System.out.println("Error. Selecciona una opción válida\n");
+                        break;
+                }
+            } catch (InputMismatchException error) {
+                System.out.println("\nDatos no válidos. Intenta de nuevo.\n");
+                sc.next();
+            } catch (NullPointerException error) {
+                System.out.println(error.getMessage());
+                sc.next();
+            } catch (Exception error) {
+                System.out.println("Entrada inválida. Intenta de nuevo.");
+                sc.next();
+            }
+        }while(flag);
     }
 
 }
